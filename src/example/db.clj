c8948e738e96af13d563b654552e9c2ba02a3624
Paul Butcher <paul@paulbutcher.com>
2025-01-24 12:09:43 +0000

    Add database support

---
+++ src/example/db.clj
@@ -0,0 +1,43 @@
+(ns example.db
+  (:require [honey.sql :as sql]
+            [honey.sql.helpers :as h]
+            [next.jdbc :as jdbc]
+            [next.jdbc.result-set :as rs]))
+
+;; Quick and dirty for now: should use migrations
+(defn create-tables!
+  [ds]
+  (jdbc/execute! ds ["
+CREATE TABLE IF NOT EXISTS messages (
+  id SERIAL PRIMARY KEY,
+  content TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)"]))
+
+(def ds
+  (delay (let [db-spec {:dbtype "postgresql"
+                        :dbname "example_lambda_app"
+                        :host (or (System/getenv "DB_HOST") "localhost")
+                        :user "postgres"
+                        :password (or (System/getenv "DB_PASSWORD") "postgres")}
+               ds (jdbc/get-datasource db-spec)]
+           (create-tables! ds)
+           ds)))
+
+(defn query
+  [honey-query]
+  (jdbc/execute! @ds
+                 (sql/format honey-query)
+                 {:builder-fn rs/as-unqualified-maps}))
+
+(defn insert-message!
+  [content]
+  (query (-> (h/insert-into :messages)
+             (h/columns :content)
+             (h/values [[content]]))))
+
+(defn get-messages
+  []
+  (query (-> (h/select :*)
+             (h/from :messages)
+             (h/order-by [:created_at :desc]))))
--- src/example/lambda_app.clj
+++ src/example/lambda_app.clj
@@ -1,23 +1,43 @@
 (ns example.lambda-app
-  (:require [compojure.core :refer [defroutes GET]]
+  (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :as route]
-            [hiccup2.core :refer [html]]
+            [hiccup2.core :refer [html raw]]
+            [example.db :as db]
             [ring.logger :refer [wrap-with-logger]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
-            [ring.middleware.params :refer [wrap-params]]))
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+
+(defn message-list
+  []
+  (str (html [:div#messages
+              (for [message (db/get-messages)]
+                [:div.message (:content message)])])))
 
 (defn index-page
   []
-  (str (html [:head [:title "HTMX Example"]
-              [:script {:src "https://unpkg.com/htmx.org@2.0.4"}]]
-             [:body [:h1 "HTMX Example"]
-              [:div#greeting {:hx-get "/greet" :hx-trigger "load"}]])))
+  (str (html
+        [:head [:title "HTMX Example"]
+         [:script {:src "https://unpkg.com/htmx.org@2.0.4"}]]
+        [:body [:h1 "HTMX Example"]
+         [:div
+          [:form#message-form
+           {:hx-post "/messages" :hx-target "#messages" :hx-swap "outerHTML"}
+           (raw (anti-forgery-field))
+           [:input#content
+            {:type "text" :name "content" :placeholder "Enter a message"}]
+           [:button {:type "submit"} "Send"]]]
+         [:div#messages-container {:hx-get "/messages" :hx-trigger "load"}]])))
 
-(defn greet [] (str (html [:div "Hello, World!"])))
+(defn add-message
+  [{:keys [content]}]
+  (when content (db/insert-message! content))
+  (message-list))
 
 (defroutes app-routes
   (GET "/" [] (index-page))
-  (GET "/greet" [] (greet))
+  (GET "/messages" [] (message-list))
+  (POST "/messages" {params :params} (add-message params))
   (route/not-found "Not Found"))
 
 (def app
